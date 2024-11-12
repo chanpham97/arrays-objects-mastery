62331b7bdfd14a0e099398d2e583533034d39504
Chan Pham <iamchanpham@gmail.com>
2024-11-12 17:02:18 +0000

    mastery template

---
+++ index.js
@@ -0,0 +1,42 @@
+// 1. Array Exercise
+// Create an array called 'hobbies' that contains at least 3 hobbies
+
+// Console log the second element of the array
+
+// Add another hobby to the end of the array
+
+// Remove the first element in the array.
+
+// Change the new first element to another hobby
+
+// 2. Object Exercise
+// Create an object called 'student' with 'name', 'grade' (number), and 'advisory'
+
+
+// Add a property called 'isEnrolled' and set it to true
+
+// Increase the grade by 1
+
+// Console log the student's grade to test.
+
+
+
+// 3. Nested Object with Array Exercise
+let library = {
+    sections: [
+        {
+            name: "Fiction",
+            books: ["To Kill a Mockingbird", "1984", "The Great Gatsby"]
+        },
+        {
+            name: "Non-Fiction",
+            books: ["Sapiens", "Educated", "Becoming"]
+        }
+    ]
+};
+
+// Console log the second fiction book by navigating the library object
+
+
+// Add another non-function book to the array
+
